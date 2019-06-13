226c98a72c7c319187629466413761c1c1ec99e9
DinhLeDuc <dinhleduc1997@gmail.com>
2019-06-13 21:17:04 +0700

    Init routes for client

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,44 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    'as'=> 'client.home.index',
+    'uses'=> 'Homecontroller@index'
+]);
+Route::get('about', [
+    'as'=> 'client.home.about',
+    'uses'=> 'Homecontroller@about'
+]);
+
+Route::group(['prefix' => 'gio-hang'], function () {
+    Route::get('', [
+        'as'=> 'client.cart.cart',
+        'uses'=> 'Cartcontroller@cart'
+    ]);
+    Route::get('thanh-toan', [
+        'as'=> 'client.cart.checkout',
+        'uses'=> 'Cartcontroller@checkout'
+    ]);
+    Route::get('hoan-thanh', [
+        'as'=> 'client.cart.complete',
+        'uses'=> 'Cartcontroller@complete'
+    ]);
 });
+Route::get('lien-he', [
+    'as'=> 'client.home.contact',
+    'uses'=> 'Homecontroller@contact'
+]);
+Route::group(['prefix' => 'shop'], function () {
+    Route::get('{id}', [
+        'as'=> 'client.product.detail',
+        'uses'=> 'Productcontroller@detail'
+    ]);
+    Route::get('', [
+        'as'=> 'client.product.shop',
+        'uses'=> 'Productcontroller@shop'
+    ]);
+    
+});
+
+
+
